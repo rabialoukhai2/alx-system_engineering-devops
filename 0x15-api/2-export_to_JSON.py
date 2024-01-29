12784d9c5989c991b89603451d8523f4f328c524
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2024-01-29 18:34:47 +0100

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,42 +1,19 @@
 #!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
-from sys import argv
-
-
-# converts dict to json
-def to_json_string(list_dictionaries):
-    """ converts dict to json """
-    if list_dictionaries is None or list_dictionaries == []:
-        return "[]"
-    return json.dumps(list_dictionaries)
-
-
-# saves json to json file
-def save_to_file(LO_dict, name):
-    """ write into a .json file """
-    with open(name + ".json", "w") as f:
-        f.write(to_json_string(LO_dict))
+import sys
 
 if __name__ == "__main__":
-    _id = argv[1]
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    base_url = "https://jsonplaceholder.typicode.com/"
-    user_url = "{}users/{}".format(base_url, _id)
-    tasks_url = "{}todos?userId={}".format(base_url, _id)
-
-    response = requests.get(user_url)
-    use_info = response.json()
-    user_name = use_info.get("username")
-
-    res = requests.get(tasks_url)
-    tasks = res.json()
-
-    _data = []
-    for task in tasks:
-        users_info = {"task": task.get("title"),
-                      "completed": task.get("completed"),
-                      "username": user_name}
-        _data.append(users_info)
-
-    save_to_file({_id: _data}, _id)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
