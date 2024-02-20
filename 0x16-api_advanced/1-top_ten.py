c0cb67c58da9f15c2869f8b05b0742fac30ab4a1
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2024-02-20 23:03:59 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
+"""prints the titles of the first 10 hot posts listed for a given subreddit.
 
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, print None.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
-
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
-    else:
-        print(None)
+    '''Return the title of the first 10 hot posts
+    listed for a given subreddit'''
+    try:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
+            (by /u/Large_Alternative_30)",
+        }
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if (response.status_code == 404):
+            print('None')
+            return 0
+        request = response.json().get('data').get('children')
+        for i in range(10):
+            print(request[i].get('data').get('title'))
+    except Exception:
+        print('None')
+        return 0
